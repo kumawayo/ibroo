20be943669e8c9a6c4b0fcfb7823bd02befc2018
Kumawayo tech <104639717+kumawayo@users.noreply.github.com>
2022-06-10 15:40:25 +0300

    Create ibroo.js
    Git clone https://github.com/kumawayo/ibroo/new/main

---
+++ ibroo.js
@@ -0,0 +1,19 @@
+# ibroo
+Chapter 1
+Waxan halkan kusharxiyaaa
+Qalalka jacelku
+Chapter2
+1. Ha is qaldiin 
+2.An lugu qaldiin
+3. Wax ha qaldiin
+Chapter 3
+1.naftada ku kalsoonow
+2.isku in aad soo kuubta wakhtigaga
+3. isku day in aad joojiso 
+3. Bey abuurka
+waa wano in taas fikir keyda
+Walala laha somaliyed 
+
+
+  //\\   {_____}
+//   \\(
